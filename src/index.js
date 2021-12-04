fbc9f322bb8a7ba0b8049ed0234afb32f7f13ec5
unknown <joanealves2011@gmail.com>
2021-12-04 17:23:55 -0300

    implementation of routes using react router dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Login from './pages/login/Login'
+
+
+// Importação Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//Renderização de páginas//
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  < BrowserRouter>
+    <Routes>
+      <Route path="/" exact={true} element={<Home />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </ BrowserRouter>
+  ,
   document.getElementById('root')
 );
