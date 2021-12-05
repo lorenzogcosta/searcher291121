674af55607f2824b0c506adebe3f7cbbb3e498f2
esthervbac <esther.v.bacelar@gmail.com>
2021-12-05 04:57:33 -0300

    Main Banner and Search Input done, need to check responsive CSS later.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,39 +5,37 @@ import Footer from '../../Components/Footer/Footer'
 
 import styles from '../Home/home.module.css'
 
+import iconSearch from '../../images/icon-search.svg'
+
 
 const Home = () => {
 
     return (
         <>
-                <NavBar layout="home" />
-                <div className={styles.homeContent}>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
-                    <h1>
-                        Home
-                    </h1>
+            <NavBar layout="home" />
+            <div className={styles.header}>
+                <div className={styles.headerContent}>
+                    <div className={styles.titles}>
+                        <h1>Encontre hashtags de maneira fácil.</h1>
+                        <h2>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo</h2>
+                    </div>
+                    <form>
+                        <button>
+                            <img src={iconSearch} alt="Icon Search" />
+                        </button>
+                        <input
+                            type="text"
+                            placeholder="Buscar..."
+                            className={styles.inputSearch}
+                            maxLength="140"
+                            required
+                        />
+                    </form>
                 </div>
-                <Footer />
+            </div>
+            <div className={styles.homeContent}>
+            </div>
+            <Footer />
         </>
     )
 
