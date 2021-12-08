1c3744d00d11f16d656e275fedd372a5b6616ac5
Joane Alves <joanealves2011@gmail.com>
2021-12-08 13:49:10 -0300

    import stylesheet about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import AboutProject from './sub-components/AboutProject';
 import AboutTeam from './sub-components/AboutTeam';
-
+import './About.css';
 const About = () => {
     return (
         <div className='about' >
