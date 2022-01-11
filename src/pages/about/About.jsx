cb0961c32d586c080f2c212f6b1ce3778ec0895f
Joane Alves <joanealves2011@gmail.com>
2022-01-11 18:36:34 -0300

    adding nav component and about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import NavBar from '../../components-utils/header/NavBar';
 import AboutProject from './sub-components/AboutProject';
 import AboutTeam from './sub-components/AboutTeam';
+import Footer from '../../components-utils/footer/Footer';
+
 import './About.css';
+
 const About = () => {
     return (
         <div className='about' >
+            <NavBar layout="home"/>
             <AboutProject/>
             <AboutTeam/>
-            
+            <Footer/>
         </div>
-       
     )
 }
 
