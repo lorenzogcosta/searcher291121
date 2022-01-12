9965a2b27e45afcfae366b25cd9cb73d852baf64
Joane Alves <joanealves2011@gmail.com>
2022-01-12 18:51:35 -0300

    Implementing team route access

--- src/pages/about/sub-components/AboutTeam.jsx
+++ src/pages/about/sub-components/AboutTeam.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 //componentes
 import AboutMembers from './AboutMembers';
@@ -9,42 +9,61 @@ import Esther from '../../../assets/profile/Esther.jpg';
 import Lorenzo from '../../../assets/profile/Lorenzo.jpg';
 
 const AboutTeam = () => {
+    const [infoTeam, setInfoTeam] = useState(null)
+
+    useEffect(() => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer key2CwkHb0CKumjuM");
+        myHeaders.append("Cookie", "brw=brwZr09Lc4XQ6bGuH");
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Equipe?fields%5B%5D=Squad&fields%5B%5D=Nome&fields%5B%5D=Email&fields%5B%5D=Descrição&fields%5B%5D=Imagem&fields%5B%5D=Github&fields%5B%5D=LinkedIn", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                const filteredTeam = result.records.filter((item) => item.fields.Squad === '291121')
+                const isNewArray = Array (filteredTeam)
+                console.log(result.records)
+                setInfoTeam(isNewArray)
+            })
+            .catch(error => console.log('error', error));
+    }, [])
+
     return (
         <section className="about-team">
-        <h2 className="about-team__title">Quem somos</h2>
-
-        <div className="about-team__members">
-
+            <h2 className="about-team__title">Quem somos</h2>
+            <div className="about-team__members">
+                {console.log(infoTeam)}
             <AboutMembers
-                foto={Esther}
-                nome="Esther Valentim Bacelar"
-                texto="Trabalhei no desenvolvimento da Home, em HTML, CSS, JS e React"
-                git="https://github.com/esthervbac"
-                email="esther.v.bacelar@gmail.com"
-                linkedin="https://www.linkedin.com/in/esther-valentim-bacelar/"
-            />
+                            //  foto={Esther}
+                            nome="Esther Valentim Bacelar"
+                            texto="Estudante - Desenvolvimento FullStack. Trabalhei no desenvolvimento da Home, em HTML, CSS, JS e React"
+                            git="https://github.com/esthervbac"
+                            email="esther.v.bacelar@gmail.com"
+                            linkedin="esther.v.bacelar@gmail.com"
+                        />
+                {/* {infoTeam.map((member) => (
+                    <>
+                        {console.log(member)}
+                        <AboutMembers
+                            //  foto={Esther}
+                            nome="Esther Valentim Bacelar"
+                            texto="Estudante - Desenvolvimento FullStack. Trabalhei no desenvolvimento da Home, em HTML, CSS, JS e React"
+                            git="https://github.com/esthervbac"
+                            email="esther.v.bacelar@gmail.com"
+                            linkedin="esther.v.bacelar@gmail.com"
+                        />
+                    </>
 
-            <AboutMembers
-                foto={Joane}
-                nome="Joane Alves Ribeiro"
-                texto="Fui responsável pela criação e estilização da sessão Sobre, em HTML, CSS, JS e React"
-                git="https://github.com/joanealves"
-                email="joanealves2011@gmail.com"
-                linkedin="https://www.linkedin.com/in/joane-alves-ribeiro/"
-            />
-
-            <AboutMembers
-                foto={Lorenzo}
-                nome="Lorenzo Costa "
-                texto="Contribui com o desenvolvimento do HTML, CSS e Java Script da página Login e página de buscas."
-                git="https://github.com/lorenzogcosta"
-                email="lorenzocostasan94@gmail.com"
-                linkedin="https://www.linkedin.com/in/lorenzogcosta/"
-            />
-          </div>
-    </section>
+                ))} */}
+            </div>
+        </section>
     );
-  
+
 }
 export default AboutTeam;
 
