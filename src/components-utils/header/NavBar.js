9091df5e80764d2047370e8256063c627ab01c60
Esther Bacelar <esther.v.bacelar@gmail.com>
2022-02-08 22:23:55 -0300

    Changing navbar js structure and css for a better responsiveness

--- src/components-utils/header/NavBar.js
+++ src/components-utils/header/NavBar.js
@@ -27,7 +27,11 @@ export default function NavBar ({ fixed, layout }) {
             className={styles.navbar} 
             style={{ position: `${fixed === 'true' ? 'fixed' : 'initial'}` }}
         >
-                <NavLink to="/" style={{ textDecoration: 'none' }} className={styles.logo}>hashtag<span>finder</span></NavLink>
+                <NavLink to="/" style={{ textDecoration: 'none' }}
+                    className={styles.linkImgHome, styles.logo}>
+                    hashtag
+                    <span>finder</span>
+                </NavLink>
 
                 {/*
                         There are 3 layouts with different buttons:
@@ -37,31 +41,50 @@ export default function NavBar ({ fixed, layout }) {
                         */}
 
                 {layout === 'home' && (
-                    <nav>
-                        <ul>
-                            <NavLink to="/sobre" style={{ textDecoration: 'none' }} className={styles.itemOne}><img src={aboutIcon} alt="About Area Icon" className={styles.aboutIcon} />&nbsp;&nbsp;&nbsp;Sobre</NavLink>
+                    <div className={styles.links_navbar}>
+                        <NavLink to="/sobre" style={{ textDecoration: 'none' }} 
+                            className={styles.itemOne}>
+                                <img src={aboutIcon} alt="About Area Icon" />
+                            Sobre
+                            </NavLink>
                             {logged ? (
-                                <NavLink to="/lista-de-buscas" style={{ textDecoration: 'none' }} className={styles.itemTwo}><img src={loginIcon} alt="Login Area Icon" className={styles.loginIcon} />&nbsp;&nbsp;&nbsp;Login</NavLink>
+                                <NavLink to="/lista-de-buscas" style={{ textDecoration: 'none' }} 
+                                className={styles.itemTwo}>
+                                    <img src={loginIcon} alt="Login Area Icon" />
+                                Login
+                                </NavLink>
                             ) : (
-                                <NavLink to="/login" style={{ textDecoration: 'none' }} className={styles.itemTwo}><img src={loginIcon} alt="Login Area Icon" className={styles.loginIcon} />&nbsp;&nbsp;&nbsp;Login</NavLink>
+                                <NavLink to="/login" style={{ textDecoration: 'none' }} 
+                                className={styles.itemTwo}>
+                                    <img src={loginIcon} alt="Login Area Icon" />
+                                Login
+                                </NavLink>
                             )}
-                        </ul>
-                    </nav> 
+                        </div>
                 )}
                 {layout === 'login' && (
-                    <nav>
-                        <ul>
-                            <NavLink to="/" style={{ textDecoration: 'none' }} className={styles.itemOneHome}><img src={homeIcon} alt="Home Icon" className={styles.homeIcon} />&nbsp;&nbsp;&nbsp;Home</NavLink>
-                        </ul>
-                    </nav>
+                    <div className={styles.link_navbar}>
+                        <NavLink to="/" style={{ textDecoration: 'none' }} 
+                            className={styles.itemOne}>
+                                <img src={homeIcon} alt="Home Icon" />
+                            Home
+                        </NavLink>
+                    </div>
                 )}
                 {layout === 'searchList' && (
-                    <nav>
-                        <ul>
-                            <NavLink to="/" style={{ textDecoration: 'none' }} className={styles.itemOneHome}><img src={homeIcon} alt="Home Area Icon" className={styles.homeIcon} />&nbsp;&nbsp;&nbsp;Home</NavLink>
-                            <NavLink to="/" style={{ textDecoration: 'none' }} className={styles.itemTwoLogOut} onClick={setLoggedStatus}><img src={logoutIcon} alt="Logout Icon" className={styles.logoutIcon} />&nbsp;&nbsp;&nbsp;Sair</NavLink>
-                        </ul>
-                    </nav>
+                    <div className={styles.links_navbar}>
+                        <NavLink to="/" style={{ textDecoration: 'none' }} 
+                            className={styles.itemOne}>
+                            <img src={homeIcon} alt="Home Area Icon" />
+                            Home
+                        </NavLink>
+                        <NavLink to="/" style={{ textDecoration: 'none' }} 
+                            className={styles.itemTwo} 
+                            onClick={setLoggedStatus}>
+                                <img src={logoutIcon} alt="Logout Icon" />
+                            Sair
+                        </NavLink>
+                    </div>
                 )}
         </div>
     );
