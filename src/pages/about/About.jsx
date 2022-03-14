23c738ac2949cba2f8fe00680aed02384587af9e
Esther Bacelar <esther.v.bacelar@gmail.com>
2022-03-14 20:56:07 -0300

    Adding fixed navbar and importing AboutProject component

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,14 +3,16 @@ import Footer from '../../../src/components-utils/footer/Footer';
 
 import NavBar from '../../components-utils/header/NavBar';
 import AboutTeam from './sub-components/AboutTeam';
+import AboutProject from './sub-components/AboutProject';
 
 import './About.css';
 
 const About = () => {
     return (
         <div className='about' >
-            <NavBar layout="home"/>
-            
+            <NavBar fixed="true" layout="home"/>
+
+            <AboutProject/>
             <AboutTeam/>
             <Footer/>
         </div>
